c5ebecf1dcf6c4f23f6b21d062f67c0ab73b21ee
trunghieu2803 <dev.hieunguyen03@gmail.com>
2024-09-24 01:35:30 +0700

    Revert "SendGmail"
    This reverts commit 59cc8d331c9ca52ecaf580829871a82a5619a797.

--- app/src/main/java/com/nhom2/appbantrasua/GUI/LoginRegisterActivity.java
+++ app/src/main/java/com/nhom2/appbantrasua/GUI/LoginRegisterActivity.java
@@ -1,13 +1,7 @@
 package com.nhom2.appbantrasua.GUI;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.Nullable;
@@ -21,15 +15,9 @@ import com.nhom2.appbantrasua.R;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
-import javax.mail.MessagingException;
+public class LoginRegisterActivity extends AppCompatActivity {
 
-public class LoginRegisterActivity extends AppCompatActivity
-{
-    Button btnNextPageRegister, btnRegister;
-    private ExecutorService executorService;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,23 +28,5 @@ public class LoginRegisterActivity extends AppCompatActivity
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        AnhXa();
-        btnNextPageRegister.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(LoginRegisterActivity.this, RegisterActivity.class);
-
-                startActivity(intent);
-            }
-        });
     }
-
-    void AnhXa(){
-        //Login
-        btnNextPageRegister = findViewById(R.id.buttonchuyentrangdangky);
-    }
-
-
-
-
 }
